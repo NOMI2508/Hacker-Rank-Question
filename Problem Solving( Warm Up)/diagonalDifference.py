f0683f999ca151ac5b8c07e2c22c84eb3d049138
naveed <f178149@cfd.nu.edu.pk>
2022-02-10 20:06:41 +0500

    Create DiagonalDifference
    left and right diagonal difference is solved.

---
+++ Problem Solving( Warm Up)/diagonalDifference.py
@@ -0,0 +1,21 @@
+
+def diagonalDifference(a):
+    # Write your code here
+    
+    Lsum=0
+    Rsum=0
+    result=0
+    for i in range(len(a)):
+        for j in range(len(a)):
+            if(i==j):
+                Lsum+=a[i][j]
+            elif(i+j)==(len(a)-1):
+                Rsum+=a[i][j]
+    return abs(Lsum-Rsum)
+    
+a=[[1,2,3],[4,5,6],[7,8,9]]
+
+
+print(diagonalDifference(a))
+
+
